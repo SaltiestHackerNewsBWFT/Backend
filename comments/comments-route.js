b0862050da7b176b18afd2a29d70232ea7c19266
Michael Rockingham <mrockingham2@gmail.com>
2020-07-26 17:13:16 -0400

    get comments working

--- comments/comments-route.js
+++ comments/comments-route.js
@@ -2,15 +2,43 @@ const axios = require('axios')
 
 const router = require('express').Router()
 
+const commentsApi = [
+    {
+        url: 'http://hn.algolia.com/api/v1/search_by_date?query=comment'
+    }
+]
+
 router.get('/', (req, res)=>{
-    const requiestOptions ={
+
+    // const getComments = commentsApi.map( async comments =>{
+
+    //     const data = await axios({
+    //         url: comments.url,
+    //         method: 'GET',
+    //         headers: {
+    //             accept: 'application/json'
+    //         }
+    //     })
+
+    //     return {
+
+    //         url: comments.url,
+    //         name:  data.author,
+    //         description: data.comment_text
+    //     }
+    // })
+
+
+
+    const requestOptions ={
         headers: {accept: 'application/json'},
     }
 
     axios
     .get('http://hn.algolia.com/api/v1/search_by_date?query=comment', requestOptions)
     .then(response =>{
-        res.status(200).json(response.data)
+
+        res.status(200).json(response.data.hits)
     })
     .catch(err =>{
         res.status(500).json({message: 'error fetching comments', error:err})
