f0175f627b67b83b06938dfe2c8b5bdb24035997
Michael Rockingham <mrockingham2@gmail.com>
2020-07-26 16:05:42 -0400

    able to access comments/setting up deployment

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const express = require('express');
 const configMiddleware = require('./middleWare/serverConfig')
 const authenticate = require('./auth/auth-middleware')
 const authRouter = require('./auth/auth-router')
+const profileRouter = require('./users/profileRoutes')
 
 const server = express()
-
 configMiddleware(server)
+
+
+ server.use('/api/auth', authRouter)
+  server.use('/api/profile', authenticate, profileRouter)
 
 server.get("/", (req, res) => {
     res.json({ api: "up" });
   });
 
-  server.use('/api/auth', authRouter)
+ 
 module.exports = server;
--- users/profileRoutes.js
+++ users/profileRoutes.js
@@ -1,1 +1,30 @@
 const router = require('express').Router()
+
+const Profile = require('./profileModel')
+
+router.get('/', (req, res) =>{
+    const {username, email} = req.body
+    Profile.find()
+    .then(profile =>{
+        res.status(200).json({data: `${profile.username, profile.email}`, jwt: req.jwt})
+    })
+    .catch(err => res.send(err))
+})
+
+router.get('/:id/comments', (req, res) =>{
+    const { id} = req.params
+    
+    Profile.findComments(id)
+    .then(comments =>{
+        if (comments.length) {
+            res.json(comments)
+        } else{
+            res.status(404).json({message: 'could not find comments'})
+        }
+    })
+    .catch(err =>{
+        res.status(500).json({message:'failed to get comments'})
+    })
+})
+
+module.exports = router
